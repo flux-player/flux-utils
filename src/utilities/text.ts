8b50d4d996fa6ffd27a4697c4dae30128b4a8968
Melvin Mupondori <melmups@outlook.com>
2020-04-03 02:09:26 +0200

    Cleaned up code

--- src/utilities/text.ts
+++ src/utilities/text.ts
@@ -1,13 +1,13 @@
-import {randomBytes} from "crypto"
+import { randomBytes } from "crypto";
 
 /**
  * Generate a random string with the specified length
  *
  * @param length
  */
-export const randomString = (length: number): string => {
-    return randomBytes(length).toString('hex');
-};
+export function randomString(length: number): string {
+  return randomBytes(length).toString("hex");
+}
 
 /**
  * Convert a node buffer to a string
@@ -15,11 +15,15 @@ export const randomString = (length: number): string => {
  * @param buffer
  * @param encoding
  */
-export const bufferToString = (buffer: Buffer, encoding = 'utf8'): string => buffer.toString(encoding);
+export function bufferToString(buffer: Buffer, encoding = "utf8"): string {
+  return buffer.toString(encoding);
+}
 
 /**
  * Removes path-illegal characters from a filename
  *
  * @param filename
  */
-export const stripIllegalCharacters = (filename: string): string => filename.replace(/[/\\?%*:|"<>]/g, '-');
+export function stripIllegalCharacters(filename: string): string {
+  return filename.replace(/[/\\?%*:|"<>]/g, "-");
+}
